56a81ebb738b395ba906d4354fe32bd639d9c294
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2024-05-26 21:34:55 -0400

    adding the order now button, and connecting it to a new html file

--- practice/myapp/views.py
+++ practice/myapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+def order(request):
+    return render(request, "order.html")
