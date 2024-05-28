5ecc7fe0b70dcbb068a0cd7d1887f5f2bd935fb9
Matt2334 <89859938+Matt2334@users.noreply.github.com>
2024-05-27 23:34:15 -0400

    Adding Backend and data verification

--- practice/myapp/models.py
+++ practice/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AddOrder(models.Model):
+    topping = models
+    quantity = models.IntegerField()
+    delivery_date = models.DateField()
+    name = models.CharField("Full Name:", max_length=40)
+    email = models.CharField("Email:", max_length=40)
+    phone = models.CharField("Phone Number:", max_length=11)
+    comments = models.TextField("Additional Comments", max_length=1000)
+    delivery_time =models.CharField("Delivery Time:", max_length=10)
